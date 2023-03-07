8bcf9e17d02a8b0f792833f1904dcf0a19811086
Masterolic <rockingmallu8@gmail.com>
2023-03-07 07:55:33 +0530

    Create Saavn.py

---
+++ mbot/plugins/Saavn.py
@@ -0,0 +1,33 @@
+from python import Client,filters
+import requests,os,wget 
+
+
+@Client.on_message(filters.command('saavn') & filters.text)
+async def song(client, message):
+    try:
+       args = message.text.split(None, 1)[1]
+    except:
+        return await message.reply("/saavn requires an argument.")
+    if args.startswith(" "):
+        await message.reply("/saavn requires an argument.")
+        return ""
+    pak = await message.reply('Downloading...')
+    try:
+        r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
+    except Exception as e:
+        await pak.edit(str(e))
+        return
+    sname = r['data']['results'][0]['name']
+    slink = r['data']['results'][0]['downloadUrl'][4]['link']
+    ssingers = r['data']['results'][0]['primaryArtists']
+  #  album_id = r.json()[0]["albumid"]
+    img = r['data']['results'][0]['image'][2]['link']
+    thumbnail = wget.download(img)
+    file = wget.download(slink)
+    ffile = file.replace("mp4", "mp3")
+    os.rename(file, ffile)
+    await pak.edit('Uploading...')
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
+    os.remove(ffile)
+    os.remove(thumbnail)
+    await pak.delete()
