e44849aea0b849a65f395f3683cb19f365e24418
Masterolic <rockingmallu8@gmail.com>
2023-03-08 12:21:35 +0530

    Update spotify.py

--- mbot/plugins/spotify.py
+++ mbot/plugins/spotify.py
@@ -199,7 +199,7 @@ async def spotify_dl(_,message):
             pass
         await message.reply_text(f"Done✅",   
          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Feedback", callback_data="feed")]]))
-        await message.reply_text(f"Check out @spotify_downloa_bot(music)  @spotifynewss(News)")
+        await message.reply_text(f"Check out @spotify_downloa (music)  @spotifynewss(News)")
 @Mbot.on_callback_query(filters.regex(r"feed"))
 async def feedback(_,query):
       await query.message.edit(f"Feedback 🏴‍☠️",
