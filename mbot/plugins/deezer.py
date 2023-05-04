ee8a4c62a90eb129162cc8367a0c9a3fb6ff1b79
Masterolic <rockingmallu8@gmail.com>
2023-05-04 11:36:06 +0530

    Update deezer.py

--- mbot/plugins/deezer.py
+++ mbot/plugins/deezer.py
@@ -26,7 +26,7 @@ from mbot import AUTH_CHATS, LOG_GROUP,Mbot
 from deezer import Client
 from os import mkdir
 from random import randint
-from mbot.utils.mainhelper import fetch_tracks,download_songs,parse_deezer_url,thumb_down
+from mbot.utils.mainhelper import fetch_tracks,download_dez,parse_deezer_url,thumb_down
 
 
 client = Client()
@@ -46,7 +46,7 @@ async def link_handler(_, message):
             mkdir(randomdir)
             for song in songs:
                 PForCopy = await message.reply_photo(song.get('cover'),caption=f"🎧 Title : `{song['name']}`\n🎤 Artist : `{song['artist']}`\n💽 Album : `{song['album']}`\n💽 Song Number : `{song['playlist_num']}`")
-                path = await download_songs(song,randomdir)
+                path = await download_dez(song,randomdir)
                 thumbnail = await thumb_down(song.get('thumb'),song.get('name'))
                 AForCopy = await message.reply_audio(path,performer=song.get('artist'),title=f"{song.get('name')} - {song.get('artist')}",caption=f"[{song['name']}](https://www.deezer.com/track/{song['deezer_id']}) | {song['album']} - {song['artist']}",thumb=thumbnail,duration=song['duration'])
                 if LOG_GROUP:
