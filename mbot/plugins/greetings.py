8929411c805c31c65ab061a881957137e2adbdfc
Masterolic <rockingmallu8@gmail.com>
2023-11-06 19:55:15 +0530

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -108,5 +108,5 @@ async def help_home(_,query):
     button = [
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
     ]
-    await query.message.edit(f"Hello **{query.from_user.first_name}**, I'm **@NeedMusicRobot**.\nI'm Here to download your music.",
-                        reply_markup=InlineKeyboardMarkup(button)
+    await query.message.edit(f"Hello **{query.from_user.first_name}**, I'm **@spotify_downloa_bot**.\nI'm Here to download your music.",
+                        reply_markup=InlineKeyboardMarkup(button))
